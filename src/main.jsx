f0ed5cd2c807d3bc41c6d5ad41438c08929694fe
Nikhil-Mani <81432060+Nikhil-Mani@users.noreply.github.com>
2023-07-25 16:58:49 -0400

    Troubleshooting 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
     <Header />
     <Routes>
-      <Route path='' element={<Home />} />
+      <Route path='/' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
     </Routes>
