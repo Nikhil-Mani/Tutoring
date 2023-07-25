58ff67fda82ee3697799c3f12da4eb2540a96ec8
Nikhil-Mani <81432060+Nikhil-Mani@users.noreply.github.com>
2023-07-25 16:51:42 -0400

    Updated Website 	new file:   src/assets/picture.jpeg 	modified:   src/index.css 	new file:   src/pages/about.css 	modified:   src/pages/about.jsx 	new file:   src/pages/contact.css 	modified:   src/pages/contact.jsx 	modified:   src/pages/header.css 	new file:   src/pages/home.css 	modified:   src/pages/home.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,26 @@
+import picture from '../assets/picture.jpeg'
+import './about.css'
 export function About()
 {
     return(
-        <h3>
-            Nikhil is a high school student who goes to Academies@Englewood
-            he has a 4.0 GPA like a nerd :))
-        </h3>
+        <>
+        <div className='about_container'>
+        <div className='image'>
+                <img id='aimg'src={picture} />
+            </div>
+           <br></br>
+            <div className="text">
+                <h3>Nikhil is a rising junior. He loves to learn and to teach. He takes a rigorous courseload (AP and Honors) while maintaining a 4.0 GPA.</h3>
+                <h3>He founded a tutoring club at his high school to help under-achieving students succeed in math He also tutors SAT and Algebra 1 online on a platform affiliated with Khan Academy called Schoolhouse</h3>
+                <h3></h3>
+                <h3>In his free time, Nikhil enjoys playing tennis, programming, and reading. </h3>
+            </div>
+            
+        </div>
+        
+        </>
+        
+        
+        
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import './home.css'
 
 export function Home()
 {
     return(
-        <h1> Need help in school? Call Nikhil! It's the rule! :))))))))</h1>
+        <div className="home_container">
+            <div className="text">
+                <h1 id='intro'>Online Tutoring With An <br />Experienced, <br />High-Performing <br />High School Student</h1>
+            </div>
+            <div className="image">
+                <img id='img' src='https://media.istockphoto.com/id/1034464980/photo/mother-helping-daughter-with-homework.jpg?s=612x612&w=0&k=20&c=ypkpyR_Sry5VZ2wAE8E2Q2qTzMqMY3V-N52VSeD5Ya0=' />
+            </div>
+            <div>
+                <h1 className="stext">Learn from a student who cares about helping you learn and grow. I offer tutoring services in a variety of subjects, such as Pre-Calculus, US History, Chemistry, or English literature. </h1>
+            </div>
+        </div>
+        
+
     )
 }
